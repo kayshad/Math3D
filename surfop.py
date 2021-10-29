f0e3e242374717c7721b0d27025ced9079190a7f
kayshad <kaisslama@gmail.com>
2021-10-29 14:24:07 +0200

    Update surfop.py

--- surfop.py
+++ surfop.py
@@ -17,13 +17,13 @@ faces  = []
 
 
 
-def surf():
-    name = 'Grid'
-    div = 1
-    rge = int(10*1/div)*2
+def surf(name, div, t):
+    name = name
+    div = div
+    rge = int(t*1/div) #*2
     rgf = rge*2
     rgfd = rgf+1
-    verts.extend([(i*div,j*div,sin(i)) for i in range(-rge,rge+1) for j in range(-rge,rge+1)])
+    verts.extend([(i*div,j*div,sin(i)+cos(j)) for i in range(-rge,rge+1) for j in range(-rge,rge+1)])
     faces.extend([i+(j*rgfd),(i+(j*rgfd)+rgfd),(i+(j*rgfd)+rgfd+1),(i+(j*rgfd)+1)] for i in range(rgf) for j in range(rgf))
     mesh = bpy.data.meshes.new(name)
     mesh.from_pydata(verts, [], faces)
@@ -33,3 +33,7 @@ def surf():
     obj = bpy.data.objects.new(name, mesh)
     obj.modifiers.new('My SubDiv', 'SUBSURF')
     bpy.context.scene.collection.objects.link(obj)
+    
+    
+    
+surf('Grid',1,10)
