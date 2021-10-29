7312339fe824aac49f2c6ac50d18afc53553ed14
kayshad <kaisslama@gmail.com>
2021-10-29 14:18:26 +0200

    Update surfop.py

--- surfop.py
+++ surfop.py
@@ -14,21 +14,22 @@ verts = []
 edges = []
 faces  = []
 
-name = 'Grid'
 
-div = 1
-rge = int(10*1/div)*2
-rgf = rge*2
-rgfd = rgf+1
-print(rge)
 
-verts.extend([(i*div,j*div,sin(i)) for i in range(-rge,rge+1) for j in range(-rge,rge+1)])
-faces.extend([i+(j*rgfd),(i+(j*rgfd)+rgfd),(i+(j*rgfd)+rgfd+1),(i+(j*rgfd)+1)] for i in range(rgf) for j in range(rgf))
-mesh = bpy.data.meshes.new(name)
-mesh.from_pydata(verts, [], faces)
-mesh.update(calc_edges=True)
-for f in mesh.polygons:
-    f.use_smooth = True
-obj = bpy.data.objects.new(name, mesh)
-obj.modifiers.new('My SubDiv', 'SUBSURF')
-bpy.context.scene.collection.objects.link(obj)
+
+def surf():
+    name = 'Grid'
+    div = 1
+    rge = int(10*1/div)*2
+    rgf = rge*2
+    rgfd = rgf+1
+    verts.extend([(i*div,j*div,sin(i)) for i in range(-rge,rge+1) for j in range(-rge,rge+1)])
+    faces.extend([i+(j*rgfd),(i+(j*rgfd)+rgfd),(i+(j*rgfd)+rgfd+1),(i+(j*rgfd)+1)] for i in range(rgf) for j in range(rgf))
+    mesh = bpy.data.meshes.new(name)
+    mesh.from_pydata(verts, [], faces)
+    mesh.update(calc_edges=True)
+    for f in mesh.polygons:
+        f.use_smooth = True
+    obj = bpy.data.objects.new(name, mesh)
+    obj.modifiers.new('My SubDiv', 'SUBSURF')
+    bpy.context.scene.collection.objects.link(obj)
